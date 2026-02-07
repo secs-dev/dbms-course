55b20aadc5774e066652c0032d8c90de2972542b
vityaman <vityaman.dev@yandex.ru>
2026-02-07 17:29:21 +0300

    Test Python linters
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- test/zhopa.py
+++ test/zhopa.py
@@ -1,21 +1,2 @@
-import zhopa
-
-def f(x):
-    return x
-def g(x):
-    return x
-
-x = zhopa
-x = 1 + '4'
-
-if __name__ == '__zhopa__':
-    for i in range(10):
-        for i in range(10):
-            for i in range(10):
-                for i in range(10):
-                    for i in range(10):
-                        for i in range(10):
-                            for i in range(10):
-                                for i in range(10):
-                                    for i in range(10):
-                                        print(zhopa)
+if __name__ == "__main__":
+    print("Hello, World!")
